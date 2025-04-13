8530a103a2594b23b45c258406c449815a8e0b25
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-13 15:28:25 +0500

    blog page with slug

---
+++ data/blogs.js
@@ -0,0 +1,27 @@
+blogs =
+    [
+        {
+            title: "How to get started with python ",
+            content: "this is content python", 
+            slug: "python-learn"
+        },
+        {
+            title: "How to get started with js",
+            content: "this is content with js",
+            slug: "js-learn"
+        },
+        {
+            title: "How to get started with java",
+            content: "this is content java ",
+            slug: "java-learn"
+        },
+        {
+            title: "How to get started with cpp",
+            content: "this is content cpp",
+            slug: "cpp-learn"
+        }
+
+    ]
+
+
+module.exports = blogs
--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const blogs = require('../data/blogs')
 
 
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../templates/index.html'))
+  })
+router.get('/blog', (req, res) => {
+    // blogs.forEach(e => {
+    //     console.log(e.title);
+        
+    // });
+    res.sendFile(path.join(__dirname, '../templates/bloghome.html'))
+  })
+router.get('/blogpost/:slug', (req, res) => {
+    myBlog = blogs.filter((e)=>{
+        e.slug = req.params.slug;
+    })
+    console.log(myBlog);
+    
+    res.sendFile(path.join(__dirname, '../templates/blogpage.html'))
   })
 
 module.exports = router
